af6d7422f903f44ea219f41b19fd6c95f8a874f3
WooSeok-03 <7458dntjr@gmail.com>
2022-09-28 02:07:57 +0900

    [REFACTOR] args 전달 부분 수정

--- app/src/main/java/com/android/mtinfo/InformationFragment.kt
+++ app/src/main/java/com/android/mtinfo/InformationFragment.kt
@@ -1,21 +1,24 @@
 package com.android.mtinfo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavArgs
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.android.mtinfo.data.model.movie.Movie
 import com.android.mtinfo.databinding.FragmentInformationBinding
-import com.android.mtinfo.databinding.FragmentMovieBinding
-import com.android.mtinfo.presentation.viewmodel.MovieViewModel
+import com.android.mtinfo.presentation.viewmodel.movie.MovieViewModel
+import com.android.mtinfo.presentation.viewmodel.tvshow.TvShowViewModel
 import com.bumptech.glide.Glide
+import java.util.function.LongFunction
 
 class InformationFragment : Fragment() {
-    private lateinit var fragmentInformationBinding: FragmentInformationBinding
-    private lateinit var movieViewModel: MovieViewModel
+    private lateinit var binding: FragmentInformationBinding
+    private val args: InformationFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,21 +28,19 @@ class InformationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentInformationBinding = FragmentInformationBinding.bind(view)
+        binding = FragmentInformationBinding.bind(view)
 
-        val args: InformationFragmentArgs by navArgs()
-        val movie = args.selectedMovie
-        showMovieInfo(movie)
+        showInfo()
+        //findNavController().popBackStack()
     }
 
-    private fun showMovieInfo(movie: Movie) {
-        movieViewModel = (activity as MainActivity).movieViewModel
-        fragmentInformationBinding.tvTitle.text = movie.title
-        fragmentInformationBinding.tvDescription.text = movie.overview
+    private fun showInfo() {
+        binding.tvTitle.text = args.title
+        binding.tvDescription.text = args.overview
 
-        val posterURL = BuildConfig.POSTER_URL + movie.poster_path
-        Glide.with(fragmentInformationBinding.ivPoster.context)
+        val posterURL = BuildConfig.POSTER_URL + args.poster
+        Glide.with(binding.ivPoster.context)
             .load(posterURL)
-            .into(fragmentInformationBinding.ivPoster)
+            .into(binding.ivPoster)
     }
 }
--- app/src/main/java/com/android/mtinfo/MovieFragment.kt
+++ app/src/main/java/com/android/mtinfo/MovieFragment.kt
@@ -1,25 +1,21 @@
 package com.android.mtinfo
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.mtinfo.databinding.FragmentMovieBinding
 import com.android.mtinfo.presentation.adapter.MovieAdapter
-import com.android.mtinfo.presentation.viewmodel.MovieViewModel
+import com.android.mtinfo.presentation.viewmodel.movie.MovieViewModel
 
 class MovieFragment : Fragment() {
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var movieAdapter: MovieAdapter
-    private lateinit var fragmentMovieBinding: FragmentMovieBinding
+    private lateinit var binding: FragmentMovieBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,13 +26,19 @@ class MovieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentMovieBinding = FragmentMovieBinding.bind(view)
+        binding = FragmentMovieBinding.bind(view)
         movieViewModel = (activity as MainActivity).movieViewModel
         movieAdapter = (activity as MainActivity).movieAdapter
         movieAdapter.setOnItemClickListener {
+            val bundle = bundleOf(
+                "key" to "movie",
+                "title" to it.title,
+                "poster" to it.poster_path,
+                "overview" to it.overview
+            )
             findNavController().navigate(
                 R.id.action_movieFragment_to_informationFragment,
-                Bundle().apply { putSerializable("selected_movie", it) }
+                bundle
             )
         }
 
@@ -45,7 +47,7 @@ class MovieFragment : Fragment() {
     }
 
     private fun initRecyclerView() {
-        fragmentMovieBinding.rvMovie.apply {
+        binding.rvMovie.apply {
             adapter = movieAdapter
             layoutManager = LinearLayoutManager(activity)
         }
